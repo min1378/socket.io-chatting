2b63901195523cf7dfffb7a4a2c22184078e9ce5
min1378 <qwes123@naver.com>
2020-06-03 00:30:29 +0900

    chatting

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+var app = require("express")();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server); //소켓 서버 설정.
+//setting cors
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+app.get("/", function (req, res) {
+  res.sendFile("Hellow Chating App Server");
+});
+//connection event handler
+io.on("connection", function (socket) {
+  // 연결 후, callback으로 socket을 받음.
+  console.log("Connect from Client: " + socket);
+  socket.on("chat", function (data) {
+    console.log("message from Client: " + data.message);
+    var rtnMessage = { message: data.message }; // 클라이언트에게 메시지를 전송한다
+    socket.broadcast.emit("chat", rtnMessage);
+  });
+});
+server.listen(3001, function () {
+  console.log("socket io server listening on port 3001");
+});
---
+++ frontend/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+import 'vue-material/dist/theme/black-green-light.css'
+import Directives from '../plugin/directives'
+import io from 'socket.io-client';
+const socket = io('http://localhost:3000');
+Vue.prototype.$socket = socket;
+Vue.use(VueMaterial)
+Vue.use(Directives)
+Vue.config.productionTip = false
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
